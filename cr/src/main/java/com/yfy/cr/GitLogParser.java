4722156090fe94334449447f82de6c2613fa1cfd
qwordy <iamyfy@163.com>
2016-09-24 23:26:57 +0800

    yfy

---
+++ cr/src/main/java/com/yfy/cr/GitLogParser.java
@@ -0,0 +1,18 @@
+package com.yfy.cr;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+/**
+ * Created by yfy on 9/24/16.
+ */
+public class GitLogParser {
+
+  private void parse() throws Exception {
+    BufferedReader br = new BufferedReader(new FileReader(""));
+    String line;
+    while ((line = br.readLine()) != null) {
+      
+    }
+  }
+}
