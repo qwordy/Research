7ee15d49864e897c691114056eabbffa652ee5c8
qwordy <iamyfy@163.com>
2016-09-25 20:13:11 +0800

    yfy

--- cr/src/main/java/com/yfy/cr/GitLogParser.java
+++ cr/src/main/java/com/yfy/cr/GitLogParser.java
@@ -1,18 +1,55 @@
 package com.yfy.cr;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.revwalk.RevCommit;
+import org.eclipse.jgit.storage.file.FileRepositoryBuilder;
+
+import java.io.File;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Iterator;
 
 /**
  * Created by yfy on 9/24/16.
  */
 public class GitLogParser {
 
-  private void parse() throws Exception {
-    BufferedReader br = new BufferedReader(new FileReader(""));
-    String line;
-    while ((line = br.readLine()) != null) {
-      
-    }
+  public void parseAll() throws Exception {
+    //parse("hadoop");
+    parse("netty");
   }
+
+  public void parse(String project) throws Exception {
+    FileRepositoryBuilder builder = new FileRepositoryBuilder();
+    Repository repo = builder.setGitDir(new File("../../" + project + "/.git"))
+        .setMustExist(true)
+        .build();
+    Git git = new Git(repo);
+    Iterable<RevCommit> log = git.log().call();
+    Iterator<RevCommit> it = log.iterator();
+
+    int count = 0;
+    while (it.hasNext()) {
+      RevCommit commit = it.next();
+      String msg = commit.getFullMessage();
+      if (filter(msg)) {
+        count++;
+        System.out.println(commit.getId());
+        System.out.println(msg);
+      }
+    }
+    System.out.println(count);
+  }
+
+  private boolean filter(String msg) {
+    Collection<String> keywords = Arrays.asList(
+        "Concurren", "concurren",
+        "Synchroniz", "synchroniz",
+        "Atomic", "atomic");
+    for (String keyword : keywords)
+      if (msg.contains(keyword)) return true;
+    return false;
+  }
+
 }
